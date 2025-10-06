754137795aa064237b92de66592e78299aa1ef18
steppingstonesqueeze <viennacoup@proton.me>
2025-10-06 11:09:42 +0530

    Simple run length encoding in R and some persistence exponents coolness

--- basic_stats/coin_toss_persistence_exponents.R
+++ basic_stats/coin_toss_persistence_exponents.R
@@ -2,7 +2,8 @@
 
 library(ggplot2)
 library(dplyr)
-
+set.seed(1234)
+EPS <- 1.0e-8 # prevent infinities #
 # Coin toss - histo of distance between successsive heads
 
 sample_data <- c("H", "T")
@@ -11,49 +12,58 @@ data <- sample(sample_data,
                N,
                replace = TRUE)
 
-# Head locations
-pos_H <- which(data == "H")
-diff_pos_H <- diff(pos_H)
+data1 <- ifelse(data == "H", 1, 0)
 
-# Table
-cat("Count table \n\n")
-table(diff_pos_H)
-Sys.sleep(2)
+runs <- rle(data1)
 
-#Histo
-h <- hist(diff_pos_H, col = "red", breaks = 20)
+results <- data.frame(
+  group = runs$values,
+  runs = runs$lengths
+)
 
-#Histo details
+head(results)
 
-cat("Histogram counts \n")
-h$counts
-cat("Histogram breaks \n")
-h$breaks
-cat("Histogram midpoints \n")
-h$mids
-cat("Histogram density \n")
-h$density
+#Histo 
+
+ggplot(
+  data = results
+) + geom_histogram(aes(x = runs, fill = as.factor(group)), alpha = 0.5, bins = 100) + 
+  ggtitle("Runs of 0s and 1s in coin tossing")
 
 # lets now examine persistence
 
 # heads fractions > 0.5 
 
-data1 <- ifelse(data == "H", 1, 0)
 heads_fraction <- cumsum(data1) / c(1:length(data1))
 head(data1)
 head(heads_fraction)
 
-# simple plot
-plot(heads_fraction, col = "blue")
-
+# heads_win defines when heads fraction > 0.5 ; then we look at ITS runs below
 heads_win <- ifelse(heads_fraction > 0.5, 1, 0)
 head(heads_win)
 
-plot(heads_win, col = "green")
 
-# Use run length encoding to look into 0s and 1s runs
+h_df <- data.frame(
+  index = c(1:length(data1)),
+  hf = heads_fraction,
+  hw = heads_win
+)
 
-runs <- rle(data1)
+ggplot(
+  data = h_df
+) + geom_point(aes(x = index, y = hf), colour = "red") + 
+  ggtitle("heads fraction for fair coin tossing")
+
+
+# plot heads win
+ggplot(
+  data = h_df
+) + geom_point(aes(x = index, y = hw), colour = "green") + 
+  ggtitle("heads fraction > 0.5 for fair coin tossing is a 1, otherwise 0")
+
+# Use run length encoding to look into 0s and 1s runs for the heads win data
+
+runs <- rle(heads_win)
 
 results <- data.frame(
   group = runs$values,
@@ -66,8 +76,13 @@ head(results)
 
 ggplot(
   data = results
-) + geom_histogram(aes(x = runs, fill = as.factor(group)), alpha = 0.5)
+) + geom_histogram(aes(x = runs, fill = as.factor(group)), alpha = 0.5, bins = 100)
 
+# Behooves us to do a log plot on this
+
+ggplot(
+  data = results
+) + geom_histogram(aes(x = log(runs+EPS), fill = as.factor(group)), alpha = 0.5)
 
 
 
